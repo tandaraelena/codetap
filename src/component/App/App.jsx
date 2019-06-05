9bfe5f2eee75a91df13f7efbd3eed3f4b35f447a
Tandara Elena <tandaraelena@yahoo.com>
2019-06-05 18:21:49 +0100

    refactor app into its own component

---
+++ src/component/App/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const CodeTapAcademy = () => {
+  return (
+    <div>
+      <h1>This is App jsx</h1>
+      <h2>This is App jsx again</h2>
+    </div>
+  );
+};
