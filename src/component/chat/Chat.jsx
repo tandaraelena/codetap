5f45426b8a256c23bf7e1b5aca34bc0fd934478b
Tandara Elena <tandaraelena@yahoo.com>
2019-06-06 12:28:22 +0100

    feat: started to style the messages one per line

--- src/component/chat/Chat.jsx
+++ src/component/chat/Chat.jsx
@@ -1,24 +1,45 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import moment from "moment";
 import {
   GET_ALL_USERS_LIST,
   GET_GENERAL_CHANNEL_HISTORY
 } from "./chatConstants";
+import { StyledChannelMessages } from "./chatStyle.js";
 
 export const Chat = () => {
   const [channelMessageList, setChannelMessageList] = useState([]);
   useEffect(() => {
     // ditch fetch
     axios.get(GET_GENERAL_CHANNEL_HISTORY).then(({ data }) => {
+      /* 
+      "client_msg_id": "0b368fa2-2e0c-466c-8c95-04a49b2f2f3c",
+      "type": "message",
+      "text": "So let’s see when can we schedule a time to do that.",
+      "user": "U1BLREAAE",
+      "ts": "1559728336.059200"
+      */
       console.log(data);
-      setChannelMessageList(data);
+      setChannelMessageList(data.messages);
     });
   }, []);
+
+  const renderChatMessages = () => {
+    return channelMessageList.map(({ text, ts, user }) => {
+      return (
+        <StyledChannelMessages>
+          <div>{user}</div>
+          <div>{text}</div>
+          <div>{moment(ts * 1000).fromNow()}</div>
+        </StyledChannelMessages>
+      );
+    });
+  };
 
   return (
     <div>
       <h1>CodeTap Members Chat</h1>
-      <pre>{JSON.stringify(channelMessageList, null, 2)}</pre>
+      {renderChatMessages()}
     </div>
   );
 };
---
+++ src/component/chat/chatStyle.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledChannelMessages = styled.div`
+  display: grid;
+  grid-gap: 0.5rem;
+  grid-template-columns: auto 1fr auto;
+  border: 1px solid black;
+  border-radius: 5px;
+  box-shadow: 5px -5px 5px 0px rgba(0, 0, 0, 0.75);
+  padding: 0.5rem 1rem;
+  margin-bottom: 1rem;
+`;
